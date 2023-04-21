50200294ef953bbbaa770facad3faa60ac3e70e3
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-21 15:19:00 -0400

    first commit

---
+++ fashionblog/src/App.js
@@ -0,0 +1,72 @@
+
+import './App.js';
+import React from 'react';
+
+function App(){
+  return (
+  <div>
+    <header>
+      <h1>Sartre's List</h1>
+    </header>
+  <h3>Better Dressed People</h3>
+  <nav>
+  <ul>
+    <li><a href="#">Women's</a></li>
+    <li><a href="#">Men's</a></li>
+    <li><a href="#">On the Street</a></li>
+    <li><a href="#">The Catwalk</a></li>
+    <li><a href="#">AdWatch</a></li>
+    <li><a href="#">About</a></li>
+  </ul>
+  </nav>
+
+
+<div>
+  <p>March 13, 2023</p>
+  <h1>On the Street in Brooklyn</h1>
+
+  <img src="https://ps-java-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg" alt="On the Street in Brooklyn" />
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut t amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut 
+
+<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut t amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut</p>
+
+<button className='continue-button'>continue...</button>
+
+<p>March 13, 2023</p>
+
+<img src="https://ps-java-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg" alt="On the Street in Brooklyn" />
+
+<p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut t amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut 
+        
+<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut t amet, consectetur adipiscing elit. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut</p>
+
+
+<button className='continue-button'>continue...</button>
+
+
+</div>
+
+<footer>
+<nav>
+  <ul>
+    <li><a href="#">Women's</a></li>
+    <li><a href="#">Men's</a></li>
+    <li><a href="#">On the Street</a></li>
+    <li><a href="#">The Catwalk</a></li>
+    <li><a href="#">AdWatch</a></li>
+    <li><a href="#">About</a></li>
+  </ul>
+  </nav>
+  <p>&copy; 2013 Valet Industries, Inc.</p>
+</footer>
+
+</div>
+  )
+}
+
+
+
+
+
+export default App;
